bbea9d1923b22412e661a63f97021e3c3e4a362f
Humaid Khan <humaidk2@gmail.com>
2020-11-23 15:57:44 +0400

    Load data using serverside props

--- project-front/pages/index.js
+++ project-front/pages/index.js
@@ -3,19 +3,43 @@ import styles from "../styles/Home.module.css"
 import Nav from "../components/Nav"
 import Profile from "../components/Profile"
 import Projects from "../components/Projects"
+import { useEffect } from "react"
 
-export default function Home() {
+export default function Home({ data }) {
     return (
         <div className={styles.container}>
             <Head>
                 <title>Projects</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Nav username="humaidk2" />
+            <Nav username={data.username} />
             <main className={styles.main}>
                 <Profile />
                 <Projects />
             </main>
         </div>
     )
+}
+export async function getServerSideProps(context) {
+    const res = await fetch("http://project-api_node_1:8080/projects/humaidk2")
+    const data = await res.json()
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+    const projects = []
+    for (let i = 0; i < data.projects.length; i++) {
+        let projectRes = await fetch(
+            `http://project-api_node_1:8080/${data.projects[i].url}`
+        )
+        let projectData = await projectRes.json()
+        await projects.push(projectData)
+    }
+    console.log(projects)
+    return {
+        props: {
+            data,
+        }, // will be passed to the page component as props
+    }
 }
